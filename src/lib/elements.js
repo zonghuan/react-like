25998a7e626153e8880a44aad1a48ee06e453dfe
huanhuan <huanhuan@xianzaishi.com>
2018-01-15 23:33:45 +0800

    elements

--- src/lib/elements.js
+++ src/lib/elements.js
@@ -1,10 +1,15 @@
 import {elements} from './var.js'
 
-export default (component)=>{
+export default (component,prop,children)=>{
+  var wrap = null
   if(typeof(component)==='function'){
-    
-  }
-  if(typeof(component)==='string'){
 
   }
+  if(typeof(component)==='string'){
+    wrap = document.createElement(component)
+  }
+  if(!wrap){
+    return
+  }
+  
 }

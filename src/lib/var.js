3a4e9c11fdc3712417e0e20d7ff9dc99ea3c1801
huanhuan <huanhuan@xianzaishi.com>
2018-03-11 19:36:13 +0800

    var

--- src/lib/var.js
+++ src/lib/var.js
@@ -1,10 +1,5 @@
 export var isArray = (arr)=>Object.prototype.toString.call(arr)==='[object Array]'
 
-// 虚拟dom的快照
-export var domSnap = (dom)=>{
-  
-}
-
 // 比对虚拟dom
 export var diff = (pre,cur)=>{
 

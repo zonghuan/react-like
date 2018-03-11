4c3fd65721614f41bc838d203ada4f2d8ecd92d5
huanhuan <huanhuan@xianzaishi.com>
2018-03-11 19:29:19 +0800

    var

--- src/lib/var.js
+++ src/lib/var.js
@@ -1,5 +1,15 @@
 export var isArray = (arr)=>Object.prototype.toString.call(arr)==='[object Array]'
 
+// 虚拟dom的快照
+export var domSnap = (dom)=>{
+  
+}
+
+// 比对虚拟dom
+export var diff = (pre,cur)=>{
+
+}
+
 export default {
   root : null
 }
